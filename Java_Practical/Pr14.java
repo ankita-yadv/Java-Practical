51b48d934bb563fed5898f2d7e48a4925cad60f8
ankita-yadv <ankita2016yadav@gmail.com>
2023-08-20 14:06:49 +0530

    need optimization

---
+++ Java_Practical/Pr14.java
@@ -0,0 +1,29 @@
+package Java_Practical;
+
+import java.util.Scanner;
+
+public class Pr14 {
+    //N PRIME NUMBERS
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = scan.nextInt();
+        prime(n);
+        scan.close();
+    }
+
+    private static void prime(int n) {
+        for (int i = 1; i < n+1; i++) {
+            Boolean flag = true;
+            for (int j = 2; j < i/2; j++) {
+                if (i%j==0) {
+                   flag = false; 
+                   break;
+                }
+            }
+            if (flag) {
+            System.out.print(i + " ");
+            }
+        }
+    }
+}
